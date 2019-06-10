fc2e41cb5ef2a2353591528b8951a429df260fa0
BrunaF <bruferratto@gmail.com>
2019-06-10 22:58:52 +0200

    save data in db

--- Commands/ImportCommand.php
+++ Commands/ImportCommand.php
@@ -1,11 +1,10 @@
 <?php
 namespace SpotApi\Commands;
-
 use Shopware\Commands\ShopwareCommand;
-
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
+use SpotApi\Models\Genre;
 
 class ImportCommand extends ShopwareCommand
 {
@@ -16,12 +15,17 @@ class ImportCommand extends ShopwareCommand
     protected function configure()
     {
         $this
-            ->setName('spotapi:import')
+            ->setName(self::$defaultName)
             ->setDescription('Import data from spotify API.')
             ->addArgument(
-                'apiKey',
+                'client_id',
                 InputArgument::REQUIRED,
-                'The api key.'
+                'The client Id.'
+            )
+            ->addArgument(
+                'client_secret',
+                InputArgument::REQUIRED,
+                'The client secret.'
             )
             ->setHelp(<<<EOF
 The <info>%command.name%</info> imports data from spotify API.
@@ -34,7 +38,69 @@ EOF
      */
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-        $apiKey = $input->getArgument('apiKey');
-        $output->writeln('<info>'.sprintf("Got apiKey: %s.", $apiKey).'</info>');
+        $clientId = $input->getArgument('client_id');
+        $clientSecret = $input->getArgument('client_secret');
+        $output->writeln('<info>Trying to get spotify token</info>');
+        $tokenData = $spotifyToken = $this->getSpotifyToken($clientId, $clientSecret);
+        echo "GOT SPOTIFY TOKEN {$spotifyToken}";
+        $output->writeln('<info>Trying to get genres</info>');
+        $this->getGenres($tokenData["access_token"]);
     }
+    private function getSpotifyToken($clientId, $clientSecret) {
+        $authString = base64_encode("{$clientId}:${clientSecret}");
+        $options = array(
+            'http' => array(
+                'header'  =>
+                    "Authorization: Basic {$authString}"
+            ,
+                'method'  => 'POST',
+                'content' => http_build_query(['grant_type'=>'client_credentials'])
+            )
+        );
+        $url = "https://accounts.spotify.com/api/token";
+        $ctx  = stream_context_create($options);
+        $result = file_get_contents($url, false, $ctx);
+        if ($result === FALSE) {
+            throw new Exception('Spotify token not retrievable');
+        }
+        return json_decode($result, true);
+    }
+    private function makeRequest($endPoint, $token) {
+        $url ="https://api.spotify.com/v1/{$endPoint}";
+        $options = array(
+            'http' => array(
+                'header'  =>
+                    "Content-type: application/json\r\n".
+                    "Accept-type: application/json\r\n".
+                    "Authorization: Bearer {$token}"
+            ,
+                'method'  => 'GET',
+            )
+        );
+        $ctx  = stream_context_create($options);
+        $result = file_get_contents($url, false, $ctx);
+        $response = json_decode($result,true);
+        return $response["genres"];
+    }
+    protected function getGenres($token) {
+        $genres = $this->makeRequest("recommendations/available-genre-seeds",$token);
+
+        /** @var \Shopware\Components\Model\ModelManager $entityManager */
+        $em = $this->container->get('models');
+        $batchSize = 100;
+        $insertCounter = 0;
+        foreach($genres as $title) {
+            $insertCounter++;
+            $genreModel = new Genre();
+            $genreModel->title = $title;
+            $em->persist($genreModel);
+            if (($insertCounter % $batchSize) === 0) {
+                $em->flush();
+                $em->clear();
+            }
+        }
+        $em->flush();
+        $em->clear();
+    }
+
 }
