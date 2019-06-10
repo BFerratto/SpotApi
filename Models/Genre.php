a9006af2c0c2bd4fd9c9130ef3d61d6e3939ff80
BrunaF <bruferratto@gmail.com>
2019-06-10 21:11:31 +0200

    create model for genre

---
+++ Models/Genre.php
@@ -0,0 +1,42 @@
+<?php
+namespace SpotApi\Models;
+
+use Shopware\Components\Model\ModelEntity,
+    Doctrine\ORM\Mapping AS ORM;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="sa_genre")
+ */
+class Genre extends ModelEntity {
+    /**
+     * @var int
+     *
+     * @ORM\Id
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\GeneratedValue(strategy="IDENTITY")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="title", type="string", nullable=false)
+     */
+    private $title;
+
+
+    public function __get($property) {
+        if (property_exists($this, $property)) {
+            return $this->$property;
+        }
+    }
+
+    public function __set($property, $value) {
+        if (property_exists($this, $property)) {
+            $this->$property = $value;
+        }
+
+        return $this;
+    }
+}
